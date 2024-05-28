96eeb6003aa3de10060d37e78045ef4537690a70
Matheus Tecchio <matheusdiastecchio@gmail.com>
2024-05-28 15:52:05 +0100

    javascript object

--- full-stack/Javascript_Level_Two/Part6_Objects_Exercise.js
+++ full-stack/Javascript_Level_Two/Part6_Objects_Exercise.js
@@ -13,7 +13,9 @@ var employee = {
 
 // Add a method called nameLength that prints out the
 // length of the employees name to the console.
-
+function nameLength(targetEmployee) {
+  console.log(targetEmployee["name"].length)
+}
 
 ///////////////////
 // PROBLEM 2 /////
@@ -23,14 +25,20 @@ var employee = {
 var employee = {
   name: "John Smith",
   job: "Programmer",
-  age: 31
+  age: 31,
+  report: function(){
+    alert("Name is: "+this.name+", Job is: "+this.job+", Age is: "+this.age)
+  }
 }
 
 // Write program that will create an Alert in the browser of each of the
 // object's values for the key value pairs. For example, it should alert:
 
 // Name is John Smith, Job is Programmer, Age is 31.
+function alert() {
+  alert(employee["name"], employee["job"], employee["age"])  
 
+}
 
 
 ///////////////////
@@ -42,6 +50,10 @@ var employee = {
   name: "John Smith",
   job: "Programmer",
   age: 31
+}
+
+function lastName(targetEmployee) { 
+  console.log(targetEmployee["name"].split(" ")[1])
 }
 
 // Add a method called lastName that prints
