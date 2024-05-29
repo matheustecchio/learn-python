472c471b6135499bda713c210a6980d47b437015
Matheus Tecchio <matheusdiastecchio@gmail.com>
2024-05-29 09:26:19 +0100

    removing javascript repositories

--- full-stack/JavaScript_Level_One/Part3.js
+++
@@ -1,4 +0,0 @@
-alert("Welcome to the metric convertor!")
-var pounds = prompt("Enter with the value in pounds: ")
-kilograms = pounds / 2.205
-alert(pounds + " pounds in kilograms is " + kilograms)
