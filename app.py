34f42c44da7ba612ec35d2e877017ddd88d55b57
Matheus Tecchio <matheusdiastecchio@gmail.com>
2024-06-20 18:11:27 +0100

    add token refreshing

--- app.py
+++ app.py
@@ -36,7 +36,18 @@ def create_app(db_url=None):
     def check_if_token_in_blocklist(jwt_header, jwt_payload):
         return jwt_payload["jti"] in BLOCKLIST
 
-
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token is not fresh.",
+                    "error": "fresh_token_required",
+                }
+            ),
+            401,
+        )
+    
     @jwt.revoked_token_loader
     def revoked_token_callback(jwt_header, jwt_payload):
         return (
--- resources/item.py
+++ resources/item.py
@@ -12,12 +12,13 @@ blp = Blueprint("Items", "items", description="Operations on items")
 
 @blp.route("/item/<int:item_id>")
 class Item(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema)
     def get(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         return item
 
-    @jwt_required()
+    @jwt_required(refresh=True)
     def delete(self, item_id):
         jwt = get_jwt()
         if not jwt.get("is_admin"):
@@ -28,7 +29,7 @@ class Item(MethodView):
         db.session.commit()
         return {"message": "Item deleted."}
 
-    @jwt_required()
+    @jwt_required(refresh=True)
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
@@ -38,7 +39,7 @@ class Item(MethodView):
             item.price = item_data["price"]
             item.name = item_data["name"]
         else:
-            item = ItemModel(id=item_id, **item_data)
+            item = ItemModel(id=item_id, **item_data) # type: ignore
 
         db.session.add(item)
         db.session.commit()
@@ -48,11 +49,12 @@ class Item(MethodView):
 
 @blp.route("/item")
 class ItemList(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema(many=True))
     def get(self):
         return ItemModel.query.all()
 
-    @jwt_required()
+    @jwt_required(refresh=True)
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
